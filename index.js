91cb840090a594a05390ae4d084ea56629d005f9
Germain Bueno <germainrafael.buenotaguariparo@externos-cl.cencosud.com>
2019-02-03 20:25:07 -0300

    implement requirements

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-/**
- * *************
- * Comience aqui
- * *************
- */
+const math = require("./math");
+const greet = require('./greetings');
+const hello = require("./greetings/hello");
+
+console.log(math.add(1, 5));
+console.log(math.divide(1, 5));
+console.log(math.multiply(1, 5));
+console.log(math.substract(1, 5));
+console.log(greet.greet("Germain"));
