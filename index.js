9ce83823fd2604bad2885495cdc6299796fe02e6
Germain Bueno <gerbless@gmail.com>
2019-02-05 23:00:20 -0300

    view

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call")
+
+//call.greet("Germain Bueno");
+
+async function callWithPromise(){
+    const fullname = await call.withPromise("Germain", "Bueno");
+    console.log(fullname);
+}
+
+callWithPromise();
