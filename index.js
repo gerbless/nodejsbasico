51d23e90ea89067adcbfb066856aacbdb1680076
Germain Bueno <gerbless@gmail.com>
2019-02-05 22:36:51 -0300

    git class

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call")
+
+call.withCallback("Germain Bueno", call.sync);
+
+call.withPromise("Germain Promesa")
+    .then(name => console.log(name))
